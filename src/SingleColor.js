047f14b5e2592bddfefd9102491e3c8ead53ed5d
finn-01 <nguyensam576@gmail.com>
2022-01-11 15:14:17 +0700

    set-up

---
+++ src/SingleColor.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+import rgbToHex from "./utils";
+
+const SingleColor = () => {
+	return <div>Hello SingleColor</div>;
+};
+
+export default SingleColor;
